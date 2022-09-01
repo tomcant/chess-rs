36eb0e6d296f54d335da84b9725cfaca13cc4e65
Tom Cant <tomcant@gmail.com>
2022-09-01 22:41:00 +0100

    Update perft position 3 to depth 6 for tests

--- src/movegen.rs
+++ src/movegen.rs
@@ -211,7 +211,7 @@ mod tests {
         #[test]
         #[ignore]
         fn perft_position_3() {
-            assert_perft_for_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 5, 674_624);
+            assert_perft_for_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 6, 11_030_083);
         }
 
         #[test]
