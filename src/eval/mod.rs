340af58434ba8036be009fb618130cce806f996f
Tom Cant <tomcant@gmail.com>
2025-11-01 01:55:39 +0000

    Normalise checkmate evals

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -1,5 +1,6 @@
 use crate::colour::Colour::*;
 use crate::position::Position;
+use crate::search::MAX_DEPTH;
 
 pub mod material;
 mod psqt;
@@ -7,7 +8,8 @@ mod psqt;
 pub const EVAL_MAX: i32 = 10_000;
 pub const EVAL_MIN: i32 = -EVAL_MAX;
 pub const EVAL_DRAW: i32 = 0;
-pub const EVAL_CHECKMATE: i32 = 9_900;
+pub const EVAL_CHECKMATE: i32 = EVAL_MAX;
+pub const EVAL_CHECKMATE_THRESHOLD: i32 = EVAL_CHECKMATE - MAX_DEPTH as i32;
 
 pub fn eval(pos: &Position) -> i32 {
     let eval = (material::eval(White, &pos.board) - material::eval(Black, &pos.board))
