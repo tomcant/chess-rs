67296daeae1e19652e24604a23acecaf40eb12ae
Tom Cant <tomcant@gmail.com>
2026-01-13 19:58:12 +0000

    Use incremental move picking instead of sorting at every node

--- src/search/quiescence.rs
+++ src/search/quiescence.rs
@@ -1,6 +1,8 @@
-use super::*;
-use crate::movegen::{generate_non_quiet_moves, is_in_check};
-use crate::piece::Piece;
+use super::{
+    movepicker::{MovePicker, MovePickerMode},
+    *,
+};
+use crate::movegen::is_in_check;
 
 pub fn search(pos: &mut Position, mut alpha: i32, beta: i32, report: &mut Report) -> i32 {
     report.nodes += 1;
@@ -16,11 +18,9 @@ pub fn search(pos: &mut Position, mut alpha: i32, beta: i32, report: &mut Report
     }
 
     let colour_to_move = pos.colour_to_move;
+    let mut move_picker = MovePicker::new(pos, MovePickerMode::NonQuiets);
 
-    let mut moves = generate_non_quiet_moves(pos);
-    order_moves(&mut moves);
-
-    for mv in moves {
+    while let Some(mv) = move_picker.pick() {
         pos.do_move(&mv);
 
         if is_in_check(colour_to_move, &pos.board) {
@@ -42,53 +42,4 @@ pub fn search(pos: &mut Position, mut alpha: i32, beta: i32, report: &mut Report
     }
 
     alpha
-}
-
-fn order_moves(moves: &mut [Move]) {
-    moves.sort_unstable_by_key(|mv| {
-        let mvv = material::PIECE_WEIGHTS[mv.captured_piece.unwrap_or(Piece::pawn(mv.piece.colour()))];
-        let lva = material::PIECE_WEIGHTS[mv.promotion_piece.unwrap_or(mv.piece)];
-        -(mvv * 100 - lva)
-    });
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::piece::Piece;
-    use crate::square::Square;
-    use crate::testing::*;
-
-    #[test]
-    fn order_moves_by_mvv_lva() {
-        let pawn_captures_pawn = make_move(Piece::WP, Square::C4, Square::B5, Some(Piece::BP));
-        let pawn_captures_queen = make_move(Piece::WP, Square::C4, Square::D5, Some(Piece::BQ));
-        let knight_captures_bishop = make_move(Piece::WN, Square::F4, Square::D3, Some(Piece::BB));
-        let knight_captures_queen = make_move(Piece::WN, Square::F4, Square::D5, Some(Piece::BQ));
-        let knight_captures_rook = make_move(Piece::WN, Square::F4, Square::G6, Some(Piece::BR));
-        let knight_captures_knight = make_move(Piece::WN, Square::F4, Square::H3, Some(Piece::BN));
-
-        let mut moves = [
-            pawn_captures_pawn,
-            pawn_captures_queen,
-            knight_captures_bishop,
-            knight_captures_queen,
-            knight_captures_rook,
-            knight_captures_knight,
-        ];
-
-        order_moves(&mut moves);
-
-        assert_eq!(
-            moves,
-            [
-                pawn_captures_queen,
-                knight_captures_queen,
-                knight_captures_rook,
-                knight_captures_bishop,
-                knight_captures_knight,
-                pawn_captures_pawn,
-            ],
-        );
-    }
 }
